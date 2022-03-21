c90490f4e91a8948079b26bf8891ed642c911385
Annerez <98201562+Annerez@users.noreply.github.com>
2022-03-21 19:28:56 +0700

    Add files via upload

---
+++ Loongg.py
@@ -0,0 +1,18 @@
+import turtle
+tao = turtle.Pen()
+tao.speed(100)
+
+def flow():
+    for i in range(10):
+        tao.circle(50)
+        tao.left(36)
+
+    tao.right(90)
+    tao.forward(250)
+flow()
+
+    
+
+
+
+    
